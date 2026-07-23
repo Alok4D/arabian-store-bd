a3727f6919fd87930be8a205f64d981a925daae8
Alok Roy <alokroy602701@gmail.com>
2026-07-23 17:03:57 +0600

    feat: initialize root layout component for the frontend application

--- arabian-store-frontend/src/app/layout.tsx
+++ arabian-store-frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
 import { ReduxProvider } from '@/components/providers/ReduxProvider';
 import Script from 'next/script';
+import { LenisProvider } from '@/components/providers/LenisProvider';
 import './globals.css';
 
 const manrope = Manrope({ 
@@ -25,7 +26,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="bn">
+      <head>
+        {/* Facebook Pixel Code - Test ID */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body className={notoSansBengali.className}>
+    <html lang="bn" className={manrope.variable}>
+      <body>
         <ReduxProvider>
           <LenisProvider>
             {children}
